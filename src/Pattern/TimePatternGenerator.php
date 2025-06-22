dfca078a9bb31c9bd614c25a6e1dace756fb9100
Igor Pinchuk <i.pinchuk.personal@gmail.com>
2025-06-22 19:18:53 +0400

    Add time pattern generator

---
+++ src/Pattern/TimePatternGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html5PatternGenerator\Pattern;
+
+class TimePatternGenerator
+{
+    /**
+     * Convert a PHP time format into a regex pattern.
+     */
+    public static function pattern(string $format = 'H:i'): string
+    {
+        $map = [
+            'H' => '(?:[01][0-9]|2[0-3])',
+            'i' => '[0-5][0-9]',
+            's' => '[0-5][0-9]',
+            ':' => ':',
+        ];
+
+        $regex = '';
+        $length = strlen($format);
+        for ($i = 0; $i < $length; $i++) {
+            $ch = $format[$i];
+            $regex .= $map[$ch] ?? preg_quote($ch, '/');
+        }
+
+        return $regex;
+    }
+}
